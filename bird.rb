d316706ed90b9190e00d1f37081dd31849d04442
Sam Craig <craigsamm@gmail.com>
2017-12-02 01:27:27 -0500

    Add the code

---
+++ bird.rb
@@ -0,0 +1,29 @@
+require "matrix"
+require "rubystats"
+require "ruby2d"
+
+class Bird
+	attr_accessor :position, :velocity
+
+	def initialize(position, direction)
+		@position = position
+		@velocity = Vector[Rubystats::NormalDistribution.new(direction[0], 0.1).rng * 10, Rubystats::NormalDistribution.new(direction[1], 0.1).rng * 10]
+		@new_velocity = nil
+	end
+
+	def update
+		@position += @velocity
+
+		if @position[0] < 0 || @position[0] > 800
+			@velocity = Vector[-@velocity[0], @velocity[1]]
+		end
+
+		if @position[1] < 0 || @position[1] > 600
+			@velocity = Vector[@velocity[0], -@velocity[1]]
+		end
+	end
+
+	def square
+		Square.new(x: @position[0], y: @position[1], size: 5, color: "black")
+	end
+end
---
+++ main.rb
@@ -0,0 +1,59 @@
+#! /usr/bin/env ruby
+
+require "ruby2d"
+require "matrix"
+require "./bird.rb"
+
+# Set up window basics
+set title: "Birds", width: 800, height: 600, background: "white"
+
+# Used as a timer (60 = 1 second)
+tick = 0
+
+# The list of BIRDS and a starting general direction
+birds = []
+direction = Vector[rand(-1.0..1.0), rand(-1.0..1.0)]
+
+# Populate birds
+300.times do
+	birds << Bird.new(Vector[rand(800), rand(600)], direction)
+end
+
+update do
+	clear
+
+	if tick % 10 == 0
+		# Decide new velocities
+		new_velocities = []
+		birds.each do |b|
+			# Find birds within 30 of this bird.
+			near_birds = birds.select do |i|
+				Math.sqrt((b.position[0] - i.position[0]) ** 2 + (b.position[1] - i.position[1]) ** 2) < 5
+			end
+
+			if near_birds.count > 0
+				new_velocities << Vector[near_birds.sum { |i| i.velocity[0] } / near_birds.count, near_birds.sum { |i| i.velocity[1] } / near_birds.count]
+			else
+				new_velocities << b.velocity
+			end
+		end
+
+		# Set new velocities
+        birds.each_with_index do |b, i|
+            b.velocity = new_velocities[i]
+        end
+
+		# Update BIRDS
+		birds.each do |b|
+			b.update
+		end
+	end
+
+	birds.each do |b|
+		b.square
+	end
+
+	tick += 1
+end
+
+show
