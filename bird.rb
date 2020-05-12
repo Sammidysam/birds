20272291d7210c373a92ed0420083599676dd0c3
Sam Craig <craigsamm@gmail.com>
2020-05-11 23:36:46 -0400

    Add complex movement functions, but averaging is not working because it was too simplified

--- bird.rb
+++ bird.rb
@@ -1,14 +1,18 @@
 require "matrix"
-require "rubystats"
 require "ruby2d"
 
-class Bird
-	attr_reader :position, :velocity
+CENTER = [400, 300]
 
-	def initialize(position, direction, verbose)
+class Bird
+	attr_reader :position, :direction, :velocity
+
+	def initialize(position, direction, strength, verbose)
 		set_direction direction
 
 		@square = Square.new(x: position[0], y: position[1], size: 2, color: "black")
+		@position = position
+
+		@strength = strength
 
 		if verbose
 			@collision_square = Circle.new(x: position[0] + 1, y: position[1] + 1, radius: 5, color: "silver")
@@ -21,11 +25,8 @@ class Bird
 		end
 	end
 
-	def get_position
-		Vector[@square.x, @square.y]
-	end
-
 	def set_direction(direction)
+		@direction = direction
 		@velocity = Vector[Math.cos(direction), Math.sin(direction)]
 	end
 
@@ -33,8 +34,42 @@ class Bird
 		@velocity = velocity
 	end
 
+	def distance_to(bird)
+		other_position = bird.position
+
+		Math.sqrt(((@position[0] - other_position[0]) ** 2) + ((@position[1] - other_position[1]) ** 2))
+	end
+
+	def direction_to(point)
+		Math.atan2(point[1] - @position[1], point[0] - @position[0])
+	end
+
+	# The bird's direction will change to a weighted average of its own direction
+	# and those around it.
+	def react_to_birds(near_birds)
+		return if near_birds.count == 0
+
+		directions = near_birds.map(&:direction)
+		average = directions.sum.fdiv(directions.size)
+
+		#new_direction = @strength * @direction + (1 - @strength) * average
+		#set_direction new_direction
+	end
+
+	def stay_in_bounds
+		if @square.x < 10 || @square.x > 790 || @square.y < 10 || @square.y > 590
+			modified_strength = @strength * 0.3
+			excess = 0.3 - modified_strength
+
+			#new_direction = modified_strength * @direction + (0.7 + excess) * direction_to(CENTER)
+			#set_direction new_direction
+		end
+	end
+
 	def update
 		@square.x += @velocity[0]
 		@square.y += @velocity[1]
+
+		@position = Vector[@square.x, @square.y]
 	end
 end
--- main.rb
+++ main.rb
@@ -30,7 +30,7 @@ end.parse!
 
 # Populate birds
 100.times do
-	birds << Bird.new(Vector[rand(800), rand(600)], rand(0..2*Math::PI), options[:verbose])
+	birds << Bird.new(Vector[rand(800), rand(600)], rand(-Math::PI..Math::PI), rand(), options[:verbose])
 end
 
 on :key_down do |event|
@@ -44,30 +44,21 @@ end
 update do
 	next if paused
 
-	if tick % 1 == 0
-		# Decide new directions
-		new_directions = []
-
+	if tick % 10 == 0
 		birds.each_with_index do |b, j|
 			# Find birds within a set distance of this bird.
-			near_birds = birds.select do |i|
-				Math.sqrt(((b.get_position[0] - i.get_position[0]) ** 2) + ((b.get_position[1] - i.get_position[1]) ** 2)) < 10 && b != i
+			near_birds = birds.select.with_index do |i, k|
+				j != k && b.distance_to(i) < 10
 			end
 
-			if near_birds.count > 0
-				new_directions << [j, (0.8 * b.velocity) + (0.2 * (near_birds.inject(Vector[0, 0]) { |sum, d| sum + d.velocity } / near_birds.count))]
-			end
+			b.react_to_birds near_birds
+			b.stay_in_bounds
 		end
+	end
 
-		# Set new velocities
-		new_directions.each do |d|
-			birds[d[0]].set_velocity d[1]
-		end
-
-		# Update BIRDS
-		birds.each do |b|
-			b.update
-		end
+	# Update BIRDS
+	birds.each do |b|
+		b.update
 	end
 
 	tick += 1
